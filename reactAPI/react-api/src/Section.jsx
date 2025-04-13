d14201ab70c278e531dc39f0e9e7ae892c9d6871
jatinparmar24 <jatinparmar2424@gmail.com>
2025-04-13 21:35:41 +0530

    finding the ways

--- reactAPI/react-api/src/Section.jsx
+++ reactAPI/react-api/src/Section.jsx
@@ -5,25 +5,27 @@ function Section(){
 
     return(
         <section className="squr">
-           <div className="div1">
 
-                <div className="subdiv1">
                     <div className="mainbox">
-                        <div></div>
-                        <div></div>
+                        <div className="maindiv1"></div>
+                        <div className="maindiv2"></div>
                     </div>
-                </div>
-                <div className="subdiv2"></div>
-
-           </div>
 
 
-           <div className="div2">
+                    <div className="div1">
 
-           <div className="subdiv3"></div>
-           <div className="subdiv4"></div>
+                        <div className="subdiv1"></div>
+                        <div className="subdiv2"></div>
 
-           </div>
+                    </div>
+
+
+                    <div className="div2">
+
+                        <div className="subdiv3"></div>
+                        <div className="subdiv4"></div>
+
+                    </div>
 
         </section>
     )
