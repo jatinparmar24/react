cc30834e72d20e05750e0c9ae519603da816d34a
jatinparmar24 <jatinparmar2424@gmail.com>
2025-04-14 22:53:20 +0530

    editing plus

--- reactAPI/react-api/src/Section.jsx
+++ reactAPI/react-api/src/Section.jsx
@@ -7,8 +7,33 @@ function Section(){
         <section className="squr">
 
                     <div className="mainbox">
-                        <div className="maindiv1"></div>
-                        <div className="maindiv2"></div>
+                        <div className="maindiv1">
+                            <form action="">
+                                <label htmlFor="">Enter Name</label>:-
+                                <input type="text" placeholder="Enter Your Name" />  <br /> <br />
+
+                                <label htmlFor="">Enter Number</label>:-
+                                <input type="number" />  <br /> <br />
+
+                                <label htmlFor="">Enter E-Mail</label>:-
+                                <input type="email" />  <br /> <br />
+
+                                <label htmlFor="">Enter Password</label>:-
+                                <input type="number" />  <br /> <br />
+
+                                <label htmlFor="">Enter C-Password</label>:-
+                                <input type="number" />  <br /> <br />
+
+                                <input type="submit" />
+                            </form>
+                        </div>
+
+
+                        <div className="maindiv2">
+                            <h1>Our Details</h1>
+                            <h2><s>Available</s></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque magnam nesciunt totam incidunt, sit ut quis rerum blanditiis debitis voluptates et beatae ullam repudiandae nostrum, autem laboriosam provident possimus pariatur explicabo officiis assumenda reprehenderit asperiores eveniet veniam? Dolorem voluptas, dignissimos aliquid aut ea vitae iure ratione eaque qui, eius veniam eligendi reprehenderit iusto, praesentium itaque corporis provident beatae iste soluta!</p>
+                        </div>
                     </div>
 
 
